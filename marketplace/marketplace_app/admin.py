d90cca0a49473be5237200733dcf2b6490dffd64
fabivelosa <fabivelosa@gmail.com>
2020-11-18 15:05:13 +0000

    Criando modelo de dados para produtos.

--- marketplace/marketplace_app/admin.py
+++ marketplace/marketplace_app/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
+from marketplace_app.models import MarketPlace_List
+
+admin.site.Register(MarketPlace_List)
--- marketplace/marketplace_app/models.py
+++ marketplace/marketplace_app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class MarketPlace_List(models.Model):
+    id_produto = models.CharField(max_length=50)
+    nome_produto = models.CharField(max_length=100)
+    qtd_estoque = models.IntegerField()
+    valor_produto = models.IntegerField()
+    foto_produto = models.ImageField()
+    ativo = models.BooleanField(default=True)
+    vendedor = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.id_produto)
+
+    class Meta:
+        dt_table = 'produtos'    
