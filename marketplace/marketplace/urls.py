07925380cc78a7a670165a8e6f6ad105f356ea51
Júlia Schubert Peixoto <juliaschubertpeixoto@gmail.com>
2020-11-19 20:14:58 -0300

    Inserção do cadastro_produtos.html

--- marketplace/marketplace/urls.py
+++ marketplace/marketplace/urls.py
@@ -19,7 +19,7 @@ from app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    #path('', include('app.urls')),
+    path('cadastro_produto/', include('app.urls')),
     path('login/', views.login_user),
     path('login/submit/', views.submit_login),
     path('', views.index),
